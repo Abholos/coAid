1f9e41ef4bc7a4880a058bb8ec7202b9a78a1fb4
MChilly <mariatsili89@gmail.com>
2020-12-08 01:51:53 +0200

    Add Carousel, fonts awesome & images

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
 //JavaScript goes here
+
+//Carousel slide
+$(document).ready(function () {
+  $("#mycarousel").carousel({ interval: 2000 });
+  $("#carousel-pause").click(function () {
+    $("#mycarousel").carousel('pause');
+  });
+  $("#carousel-play").click(function () {
+    $("#mycarousel").carousel('cycle');
+  });
+});
